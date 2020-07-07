e371713b13b54941a593e3e46038a161f35600ee
vitorbraganca <vitor_braganca@unigranrio.br>
2020-07-07 00:05:08 -0300

    Primeiro commit (esqueci)
    Já está avançado, sistema de seleção de alternativas, verificação de resposta correta OK, telas, sendo, tela inicial, tela de jogo e tela final, esta com itens a serem acertados. Acompanhar via Trello.

---
+++ js/game.js
@@ -0,0 +1,167 @@
+
+let perguntas = [
+    {
+        pergunta: 'Qual a capital do Brasil?',
+        opcoes: {
+            0: 'A) Rio de Janeiro',
+            1: 'C) São Paulo',
+            2: 'B) Brasília',
+            3: 'D) Bahia',
+        },
+
+        resposta: 2,
+
+    },
+    {
+        pergunta: 'Qual a capital do Chile?',
+        0: 'A) Santiago',
+        1: 'C) Aruba',
+        2: 'B) San Chile',
+        3: 'D) Montevideu',
+        resposta: 1,
+    }
+];
+
+console.log('Carreguei');
+
+
+// Botoes
+const btnInicio = document.getElementById("pronto-btn");
+const btnDesistir = document.getElementById("desistir");
+const btnVerifica = document.getElementById("btn-verifica");
+
+// Telas
+const telaInicial = document.getElementById("tela-inicial");
+const telaQuiz = document.getElementById("tela-quiz");
+const telaFinal = document.getElementById("tela-final");
+
+// Mostradores de informacao
+let numeroPergunta = document.getElementById('pergunta-numero');
+let dificuldade = document.getElementById("dificuldade");
+let premioAtual = document.getElementById("premio-atual");
+let premioDerrota = document.getElementById("premio-derrota");
+let premioProx = document.getElementById("premio-proximo");
+let resultado = document.getElementById("resultado");
+let enunciado = document.getElementById("pergunta");
+
+// Alternativas
+
+let alternativas = document.getElementsByClassName("alternativa");
+
+console.log(alternativas);
+
+let alternativaSelecionada = "";
+
+for (let i = 0; i < alternativas.length; i++) {
+    console.log(alternativas[i].id);
+    alternativas[i].addEventListener("click", evt => {
+        if (alternativaSelecionada === evt.target) return
+        if (alternativaSelecionada === "") {
+            alternativaSelecionada = evt.target;
+
+            console.log('Alternativa: ' + evt.target);
+            // let selecionada = document.getElementById(alternativaSelecionada);
+            // console.log(selecionada);
+            alternativaSelecionada.classList.toggle('alternativa');
+            alternativaSelecionada.classList.toggle('selecionada');
+
+            Number(alternativaSelecionada);
+            console.log(alternativaSelecionada);
+        } else {
+            alternativaSelecionada.classList.toggle('alternativa');
+            alternativaSelecionada.classList.toggle('selecionada');
+
+            alternativaSelecionada = evt.target;
+
+            console.log('Alternativa: ' + evt.target);
+            // let selecionada = document.getElementById(alternativaSelecionada);
+            alternativaSelecionada.classList.toggle('alternativa');
+            alternativaSelecionada.classList.toggle('selecionada');
+
+            Number(alternativaSelecionada);
+            console.log(alternativaSelecionada);
+        }
+    });
+}
+
+btnInicio.addEventListener( "click", ev => {
+    ev.preventDefault();
+    telaInicial.classList.add('inativo');
+    telaQuiz.classList.add('ativo');
+
+});
+
+btnDesistir.addEventListener("click", finalizar);
+
+// btnVerifica.addEventListener("click", verifica);
+
+async function verifica() {
+
+    const respostaAtual = perguntas[0].resposta;
+    Number(alternativaSelecionada);
+    console.log('Voce selecionou: ' + alternativaSelecionada);
+    console.log(typeof alternativaSelecionada);
+    console.log('Resposta atual: ' + respostaAtual);
+    console.log(typeof respostaAtual);
+
+    const opcao = Number(alternativaSelecionada.id);
+
+    if (opcao === respostaAtual) {
+        resultado.classList.remove('inativo');
+        resultado.innerText = 'Parabéns! Você acertou!';
+        await new Promise(r => setTimeout(r, 2000));
+        numeroPergunta += 1;
+        geraPergunta();
+        resultado.classList.add('ativo');
+
+    } else if (opcao !== respostaAtual && alternativaSelecionada !== "") {
+        console.log('errou');
+        resultado.classList.remove('inativo');
+        resultado.innerText = 'Que pena! Você errou!';
+        resultado.classList.add('ativo');
+        finalizar();
+
+    } else if (alternativaSelecionada === "") {
+        alert('Selecione uma alternativa!')
+        resultado.classList.remove('inativo');
+        resultado.classList.add('ativo');
+        resultado.innerText = "Você não selecionou nenhuma alternativa!";
+    }
+}
+
+function iniciar() {
+    numeroPergunta = 1;
+    if (numeroPergunta === 1) {
+        dificuldade.innerText = 'Facil';
+        premioAtual.innerText = 'R$1.000,00'
+        premioDerrota.innerText = 'R$0,00'
+        premioProx.innerText = 'R$5.000,00'
+    }
+
+    enunciado.innerText = perguntas[0].pergunta;
+    for (let i = 0; i < alternativas.length; i++) {
+        alternativas[i].innerHTML = perguntas[0].opcoes[i];
+    }
+
+}
+
+function geraPergunta() {
+    if (numeroPergunta === 2) {
+        dificuldade.innerText = 'Fácil';
+        premioAtual.innerText = 'R$5.000,00'
+        premioDerrota.innerText = 'R$1.000,00'
+        premioProx.innerText = 'R$25.000,00'
+    }
+}
+
+function finalizar() {
+    // alert('O jogo acabou! :)')
+    telaInicial.classList.remove('ativo');
+    telaInicial.classList.add('inativo');
+    telaQuiz.classList.remove('ativo');
+    telaQuiz.classList.add('inativo');
+    telaFinal.classList.add('ativo');
+}
+
+iniciar();
+
