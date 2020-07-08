b40fab7405d369df1f7eb858477e9759170df0f4
vitorbraganca <vitor_braganca@unigranrio.br>
2020-07-07 23:28:11 -0300

    Refatoracao
    Adicionando comentários ao código.

--- js/game.js
+++ js/game.js
@@ -1,3 +1,8 @@
+/* Array de objetos para as informações,
+* contendo o número identificador do enunciado, prêmio atual
+* prêmio caso erre a pergunta e o próximo prêmio.
+ */
+
 let informacoes = [
     {
         numero: 1,
@@ -42,32 +47,34 @@ let informacoes = [
         premioProx: 'VITÓRIA',
     }
 ]
-
+/* Array de objetos para as perguntas, contendo:
+*  o enunciado, as opcoes, a dificuldade e a resposta correta.
+*/
+// LEMBRAR DE ENCONTRAR UMA FORMA ESCONDER ISSO DO CLIENTE (modulos parece uma boa opção)
 let perguntas = [
     {
         pergunta: 'Qual a capital do Brasil?',
         opcoes: {
-            0: 'A) Rio de Janeiro',
+            0: 'A)  Rio de Janeiro',
             1: 'C) São Paulo',
             2: 'B) Brasília',
             3: 'D) Bahia',
         },
         repetida: '',
         resposta: 2,
-
     },
 
     {
+        // TROCAR ESSA PERGUNTA -> ESTÁ ERRADA!
         pergunta: 'Qual a capital do Peru?',
         opcoes: {
             0: 'A) Santiago',
-            1: 'C) Aruba',
-            2: 'B) San Chile',
+            1: 'C) Lima',
+            2: 'B) Cuzco',
             3: 'D) Montevideu',
         },
-
         repetida: '',
-        resposta: 0,
+        resposta: 1,
     },
 
     {
@@ -82,12 +89,7 @@ let perguntas = [
         repetida: '',
         resposta: 0,
     }
-
 ];
-
-
-console.log('Carreguei');
-
 
 // Botoes
 const btnInicio = document.getElementById("pronto-btn");
@@ -118,6 +120,7 @@ let premios = [
     document.getElementById("premio-5"),
     document.getElementById("premio-6")
 ]
+
 let resultadoPerguntas = document.getElementById("resultado-perguntas");
 let resultadoPremio = document.getElementById("resultado-premio");
 
@@ -126,74 +129,53 @@ let resultadoPremio = document.getElementById("resultado-premio");
 
 let alternativas = document.getElementsByClassName("alternativa");
 
-console.log(alternativas);
+// console.log(alternativas);
 
 let alternativaSelecionada = "";
 
 btnInicio.addEventListener( "click", ev => {
     ev.preventDefault();
-    console.log('Iniciando');
+    // console.log('Iniciando');
+
+    // COLOCAR NUMA FUNÇÃO
     telaInicial.classList.add('inativo');
     telaQuiz.classList.add('ativo');
-
 });
 
 btnDesistir.addEventListener("click", finalizar);
 
 btnVerifica.addEventListener("click", verifica);
 
-// btnVerifica.addEventListener("click", verifica);
-
-async function verifica() {
-
-    Number(alternativaSelecionada);
-
-    const opcao = Number(alternativaSelecionada.id);
-    console.log('Opcao ' + opcao);
-
-    // RESPOSTA ATUAL NÃO ESTÁ SENDO ENCONTRADA
-    if (opcao === respostaAtual) {
-        resultado.classList.remove('inativo');
-        resultado.innerText = 'Parabéns! Você acertou!';
-        await new Promise(r => setTimeout(r, 2000));
-        numeroPergunta += 1;
-        resultado.classList.add('ativo');
-        if (numeroPergunta > 6) finalizar();
-        geraPergunta();
-
-    } else if (opcao !== respostaAtual && alternativaSelecionada !== "") {
-        console.log('errou');
-        resultado.classList.remove('inativo');
-        resultado.innerText = 'Que pena! Você errou!';
-        resultado.classList.add('ativo');
-        finalizar();
-
-    } else if (alternativaSelecionada === "") {
-        alert('Selecione uma alternativa!')
-        resultado.classList.remove('inativo');
-        resultado.classList.add('ativo');
-        resultado.innerText = "Você não selecionou nenhuma alternativa!";
-    }
-}
-
+// Função usada para iniciar o jogo.
 function iniciar() {
     numeroPergunta = 1;
     insereInfo(numeroPergunta);
-
     const respostaAtual = geraPergunta();
     console.log('Nova resposta atual :' + respostaAtual);
-    // enunciado.innerText = perguntas[0].pergunta;
-    // for (let i = 0; i < alternativas.length; i++) {
-    //     alternativas[i].innerHTML = perguntas[0].opcoes[i];
-    // }
-
 }
 
+// Função usada para randomizar um indice para ser usado na seleção de pergunta
+function randomiza(range) {
+    let indice;
+    return indice = Math.floor(Math.random() * range);
+}
+// Função usada para verificar e garantir que nenhuma pergunta seja repetida.
+function verificaRepeticao() {
+    let tentativas = 0;
+    while (true) {
+        let indice = randomiza(perguntas.length);
+        tentativas += 1;
+        if (perguntas[indice].repetida === '') {
+            // Setando pergunta como repetida
+            perguntas[indice].repetida = 'sim';
+            return indice
+        } else if (tentativas > 15) return indice = 999;
+    }
+}
+
+// Função usada para geração de pergunta
 function geraPergunta() {
     insereInfo(numeroPergunta);
-
-
-
     let indice = verificaRepeticao();
     if (indice === 999) {
         enunciado.innerText = 'Acho que acabaram minhas perguntas... Recarregue a página!'
@@ -235,58 +217,7 @@ function geraPergunta() {
     return respostaAtual = perguntas[indice].resposta;
 }
 
-function randomiza(range) {
-    let indice;
-    return indice = Math.floor(Math.random() * range);
-}
-
-function verificaRepeticao() {
-    let tentativas = 0;
-    while (true) {
-        let indice = randomiza(perguntas.length);
-        tentativas += 1;
-        if (perguntas[indice].repetida === '') {
-            // Setando pergunta como repetida
-            perguntas[indice].repetida = 'sim';
-            return indice
-        } else if (tentativas > 15) return indice = 999;
-    }
-}
-
-
-function finalizar() {
-    // alert('O jogo acabou! :)')
-    telaInicial.classList.remove('ativo');
-    telaInicial.classList.add('inativo');
-    telaQuiz.classList.remove('ativo');
-    telaQuiz.classList.add('inativo');
-    telaFinal.classList.add('ativo');
-    alert(numeroPergunta-1);
-
-    resultadoPerguntas.innerText = `Você conseguiu responder ${numeroPergunta-1} de 6 perguntas e faturou...`;
-    if (numeroPergunta-1 === 0) resultadoPremio.innerText = `R$0,00 reais! Que pena :(`;
-    else if (numeroPergunta >= 6) resultadoPremio.innerText = `R$1.000.000,00 de reais! Parabéns!`;
-
-    resultadoPremio.innerText = `${informacoes[numeroPergunta-1].premioDerrota} reais!`;
-
-}
-
-function alterarEstilo() {
-    console.log('limpando ' + alternativaSelecionada.id)
-    alternativaSelecionada.classList.toggle('alternativa');
-    alternativaSelecionada.classList.toggle('selecionada');
-}
-
-function limpar() {
-    if (alternativaSelecionada) {
-        alterarEstilo();
-        alternativaSelecionada = "";
-    }
-
-    resultado.classList.remove('ativo');
-    resultado.classList.add('inativo');
-}
-
+// Função usada para inserir informação nos campos de informação da tela.
 function insereInfo(numeroPergunta) {
     // Limpa alternativa selecionada
     if (alternativaSelecionada) limpar();
@@ -301,5 +232,67 @@ function insereInfo(numeroPergunta) {
     console.log('premio selecionado: ' + premios[numeroPergunta-1])
     premios[numeroPergunta-1].setAttribute("id","premio-selecionado");
 }
+
+// Função usada para comparar a resposta do usuário com a resposta da pergunta.
+async function verifica() {
+
+    const opcao = Number(alternativaSelecionada.id);
+
+    if (opcao === respostaAtual) {
+        resultado.classList.remove('inativo');
+        resultado.innerText = 'Parabéns! Você acertou!';
+        await new Promise(r => setTimeout(r, 2000));
+        numeroPergunta += 1;
+        resultado.classList.add('ativo');
+        if (numeroPergunta > 6) finalizar();
+        geraPergunta();
+
+    } else if (opcao !== respostaAtual && alternativaSelecionada !== "") {
+        console.log('errou');
+        resultado.classList.remove('inativo');
+        resultado.innerText = 'Que pena! Você errou!';
+        resultado.classList.add('ativo');
+        finalizar();
+
+    } else if (alternativaSelecionada === "") {
+        alert('Selecione uma alternativa!')
+        resultado.classList.remove('inativo');
+        resultado.classList.add('ativo');
+        resultado.innerText = "Você não selecionou nenhuma alternativa!";
+    }
+}
+
+function alterarEstilo() {
+    console.log('limpando ' + alternativaSelecionada.id)
+    alternativaSelecionada.classList.toggle('alternativa');
+    alternativaSelecionada.classList.toggle('selecionada');
+}
+
+// Função usada ao encerrar o jogo.
+// FALTA ADICIONAR UMA FORMA DE RETORNAR AO INICIO!!!
+function finalizar() {
+    // ADICIONAR UMA FUNCAO DEPOIS (codigo repetido)
+    telaInicial.classList.remove('ativo');
+    telaInicial.classList.add('inativo');
+    telaQuiz.classList.remove('ativo');
+    telaQuiz.classList.add('inativo');
+    telaFinal.classList.add('ativo');
+    alert(numeroPergunta-1);
+    resultadoPerguntas.innerText = `Você conseguiu responder ${numeroPergunta-1} de 6 perguntas e faturou...`;
+    if (numeroPergunta-1 === 0) resultadoPremio.innerText = `R$0,00 reais! Que pena :(`;
+    else if (numeroPergunta >= 6) resultadoPremio.innerText = `R$1.000.000,00 de reais! Parabéns!`;
+    resultadoPremio.innerText = `${informacoes[numeroPergunta-1].premioDerrota} reais!`;
+}
+
+// Função usada pra limpar a alternativa selecionada
+function limpar() {
+    if (alternativaSelecionada) {
+        alterarEstilo();
+        alternativaSelecionada = "";
+    }
+    resultado.classList.remove('ativo');
+    resultado.classList.add('inativo');
+}
+
 
 iniciar();
