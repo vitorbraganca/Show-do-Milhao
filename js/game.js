b68a1d3a15c4e57024bac6806f63c514ab8a3942
vitorbraganca <vitor_braganca@unigranrio.br>
2020-07-08 22:58:10 -0300

    Atualizacoess e Refatoracao
    # Atualizacoes:
    - Agora é possível ver qual era a resposta correta em caso de erro.
    - Novas perguntas adicionadas.
    
    # Refatoracao
    - Objeto que identifica se a pergunta ja foi selecionada alguma vez agora e do tipo boolean;
    - Arrays e variaveis sem uso removidas do codigo.

--- js/game.js
+++ js/game.js
@@ -9,43 +9,99 @@ let informacoes = [
         dificuldade: 'Fácil',
         premioAtual: 'R$1.000,00',
         premioDerrota: 'R$0,00',
-        premioProx: 'R$5.000,00',
+        premioProx: 'R$1.500,00',
     },
     {
         numero: 2,
         dificuldade: 'Fácil',
-        premioAtual: 'R$5.000,00',
+        premioAtual: 'R$1.500,00',
         premioDerrota: 'R$1.000,00',
-        premioProx: 'R$10.000,00',
+        premioProx: 'R$2.500,00',
     },
     {
         numero: 3,
-        dificuldade: 'Média',
-        premioAtual: 'R$25.000,00',
-        premioDerrota: 'R$5.000,00',
-        premioProx: 'R$50.000,00',
+        dificuldade: 'Fácil',
+        premioAtual: 'R$2.500,00',
+        premioDerrota: 'R$1.500,00',
+        premioProx: 'R$5.000,00',
     },
     {
         numero: 4,
         dificuldade: 'Média',
-        premioAtual: 'R$50.000,00',
-        premioDerrota: 'R$25.000,00',
-        premioProx: 'R$100.000,00',
+        premioAtual: 'R$5.000,00',
+        premioDerrota: 'R$2.500,00',
+        premioProx: 'R$10.000,00',
     },
     {
         numero: 5,
-        dificuldade: 'Difícil',
-        premioAtual: 'R$500.000,00',
-        premioDerrota: 'R$100.000,00',
-        premioProx: 'R$1.000.000,00',
+        dificuldade: 'Média',
+        premioAtual: 'R$15.000,00',
+        premioDerrota: 'R$10.000,00',
+        premioProx: 'R$20.000,00',
     },
     {
         numero: 6,
+        dificuldade: 'Média',
+        premioAtual: 'R$20.000,00',
+        premioDerrota: 'R$15.000,00',
+        premioProx: 'R$30.000,00',
+    },
+    {
+        numero: 7,
+        dificuldade: 'Média',
+        premioAtual: 'R$30.000,00',
+        premioDerrota: 'R$20.000,00',
+        premioProx: 'R$50.000,00',
+    },
+    {
+        numero: 8,
+        dificuldade: 'Média',
+        premioAtual: 'R$50.000,00',
+        premioDerrota: 'R$30.000,00',
+        premioProx: 'R$75.000,00',
+    },
+    {
+        numero: 9,
+        dificuldade: 'Média',
+        premioAtual: 'R$75.000,00',
+        premioDerrota: 'R$50.000,00',
+        premioProx: 'R$100.000,00',
+    },
+    {
+        numero: 10,
         dificuldade: 'Difícil',
-        premioAtual: 'R$1.000.000,00',
-        premioDerrota: 'R$0,00',
-        premioProx: 'VITÓRIA',
-    }
+        premioAtual: 'R$100.000,00',
+        premioDerrota: 'R$75.000,00',
+        premioProx: 'R$200.000,00',
+    },
+    {
+        numero: 11,
+        dificuldade: 'Difícil',
+        premioAtual: 'R$200.000,00',
+        premioDerrota: 'R$100.000,00',
+        premioProx: 'R$300.000,00',
+    },
+    {
+        numero: 12,
+        dificuldade: 'Difícil',
+        premioAtual: 'R$300.000,00',
+        premioDerrota: 'R$200.000,00',
+        premioProx: 'R$400.000,00',
+    },
+    {
+        numero: 13,
+        dificuldade: 'Difícil',
+        premioAtual: 'R$400.000,00',
+        premioDerrota: 'R$300.000,00',
+        premioProx: 'R$500.000,00',
+    },
+    {
+        numero: 14,
+        dificuldade: 'Difícil',
+        premioAtual: 'R$500.000,00',
+        premioDerrota: 'R$400.000,00',
+        premioProx: 'R$1.000.000,00',
+    },
 ]
 /* Array de objetos para as perguntas, contendo:
 *  o enunciado, as opcoes, a dificuldade e a resposta correta.
@@ -60,8 +116,8 @@ let perguntas = [
             2: 'B) Brasília',
             3: 'D) Bahia',
         },
-        repetida: '',
         resposta: 2,
+        repetida: false,
     },
 
     {
@@ -72,8 +128,164 @@ let perguntas = [
             2: 'B) Cuzco',
             3: 'D) Montevideu',
         },
-        repetida: '',
         resposta: 1,
+        repetida: false,
+    },
+
+    {
+        pergunta: 'Qual animação exibe uma princesa que perdeu seu sapatinho de Cristal?',
+        opcoes: {
+            0: 'A) Branca de Neve',
+            1: 'C) Cinderella',
+            2: 'B) Rapunzel',
+            3: 'D) Pocahontas',
+        },
+        resposta: 1,
+        repetida: false,
+    },
+
+    {
+        pergunta: 'Banana, morango e limão são exemplos de:',
+        opcoes: {
+            0: 'A) Frutas',
+            1: 'C) Hortaliças',
+            2: 'B) Verduras',
+            3: 'D) Tubérculos',
+        },
+        resposta: 0,
+        repetida: false,
+    },
+
+    {
+        pergunta: 'A personagem Sininho é uma: ',
+        opcoes: {
+            0: 'A) Sereia',
+            1: 'C) Princesa',
+            2: 'B) Fada',
+            3: 'D) Rainha',
+        },
+        resposta: 2,
+        repetida: false,
+    },
+
+    {
+        pergunta: 'O sapo é da classo dos:',
+        opcoes: {
+            0: 'A) Herbívoros',
+            1: 'C) Anfíbios',
+            2: 'B) Répteis',
+            3: 'D) Mamíferos',
+        },
+        resposta: 1,
+        repetida: false,
+    },
+
+    {
+        pergunta: 'A cobra é da classe dos:',
+        opcoes: {
+            0: 'A) Mamíferos',
+            1: 'C) Répteis',
+            2: 'B) Anfíbios',
+            3: 'D) Herbívoros',
+        },
+        resposta: 1,
+        repetida: false,
+    },
+
+    {
+        pergunta: 'A personagem Ariel é uma:',
+        opcoes: {
+            0: 'A) Bruxa',
+            1: 'C) Dançarina',
+            2: 'B) Feiticeira',
+            3: 'D) Sereia',
+        },
+        resposta: 3,
+        repetida: false,
+    },
+
+    {
+        pergunta: 'Em que dia comemora-se o dia da independência do Brasil?',
+        opcoes: {
+            0: 'A) 20 de novembro',
+            1: 'C) 7 de setembro',
+            2: 'B) 11 de setembro',
+            3: 'D) 25 de março',
+        },
+        resposta: 1,
+        repetida: false,
+    },
+
+/*    {
+        pergunta: '',
+        opcoes: {
+            0: 'A) ',
+            1: 'C) ',
+            2: 'B) ',
+            3: 'D) ',
+            resposta: 1,
+        },
+        repetida: false,
+    },*/
+
+    {
+        pergunta: 'Quantos países fazem parte da América Latina?',
+        opcoes: {
+            0: 'A) 15 países',
+            1: 'C) 19 países',
+            2: 'B) 20 países',
+            3: 'D) 40 países',
+        },
+        resposta: 2,
+        repetida: false,
+    },
+
+    {
+        pergunta: 'Quem nasceu de 21/05 a 21/06 pertence a qual signo?',
+        opcoes: {
+            0: 'A) Gêmeos',
+            1: 'C) Sagitário',
+            2: 'B) Câncer',
+            3: 'D) Leão',
+        },
+        resposta: 0,
+        repetida: false,
+    },
+
+    {
+        pergunta: 'Quantos continentes existem na Terra?',
+        opcoes: {
+            0: 'A) 3 continentes',
+            1: 'C) 4 continentes',
+            2: 'B) 5 continentes',
+            3: 'D) 6 continentes',
+        },
+        resposta: 3,
+        repetida: false,
+    },
+
+    {
+        pergunta: 'Qual o maior ser vivo da Terra?',
+        opcoes: {
+            0: 'A) Baleia azul',
+            1: 'C) Elefante',
+            2: 'B) Fungos',
+            3: 'D) Tubarão branco',
+        },
+        resposta: 2,
+        repetida: false,
+    },
+
+    {
+        pergunta: 'A azeitona é um:',
+        opcoes: {
+            0: 'A) Fruto',
+            1: 'C) Legume',
+            2: 'B) Vegetal',
+            3: 'D) Tempero',
+        },
+        resposta: 0,
+        repetida: false,
     },
 
     {
@@ -84,8 +296,8 @@ let perguntas = [
             2: 'B) Girafa',
             3: 'D) Dinossauro',
         },
-        repetida: '',
         resposta: 1,
+        repetida: false,
     },
 
     {
@@ -96,8 +308,8 @@ let perguntas = [
             2: 'B) Charles Duke',
             3: 'D) Buzz Aldrin',
         },
-        repetida: '',
         resposta: 1,
+        repetida: false,
     },
 
     {
@@ -108,8 +320,8 @@ let perguntas = [
             2: 'B) Michelangelo',
             3: 'D) Leonardo da Vinci',
         },
-        repetida: '',
         resposta: 2,
+        repetida: false,
     },
 
     {
@@ -120,8 +332,8 @@ let perguntas = [
             2: 'B) 1943',
             3: 'D) 1945',
         },
-        repetida: '',
         resposta: 3,
+        repetida: false,
     },
 
     {
@@ -132,8 +344,8 @@ let perguntas = [
             2: 'B) 1943',
             3: 'D) 1945',
         },
-        repetida: '',
         resposta: 0,
+        repetida: false,
     },
 
     {
@@ -144,8 +356,8 @@ let perguntas = [
             2: 'B) Egito',
             3: 'D) Dubai',
         },
-        repetida: '',
         resposta: 2,
+        repetida: false,
     },
 
     {
@@ -156,8 +368,8 @@ let perguntas = [
             2: 'B) Inércia',
             3: 'D) Ação e Reação',
         },
-        repetida: '',
         resposta: 3,
+        repetida: false,
     },
 
     {
@@ -168,9 +380,8 @@ let perguntas = [
             2: 'B) San Chile',
             3: 'D) Montevideu',
         },
-
-        repetida: '',
         resposta: 0,
+        repetida: false,
     }
 ];
 
@@ -195,21 +406,11 @@ let resultado = document.getElementById("resultado");
 let enunciado = document.getElementById("pergunta");
 
 // Mostradores de prêmio
-let premios = [
-    document.getElementById("premio-1"),
-    document.getElementById("premio-2"),
-    document.getElementById("premio-3"),
-    document.getElementById("premio-4"),
-    document.getElementById("premio-5"),
-    document.getElementById("premio-6")
-]
-
 let resultadoPerguntas = document.getElementById("resultado-perguntas");
 let resultadoPremio = document.getElementById("resultado-premio");
 
 
 // Alternativas
-
 let alternativas = document.getElementsByClassName("alternativa");
 
 let alternativaSelecionada = "";
@@ -231,6 +432,7 @@ function iniciar() {
     numeroPergunta = 1;
     insereInfo(numeroPergunta);
     const respostaAtual = geraPergunta();
+
 }
 
 // Função usada para randomizar um indice para ser usado na seleção de pergunta
@@ -244,9 +446,9 @@ function verificaRepeticao() {
     while (true) {
         let indice = randomiza(perguntas.length);
         tentativas += 1;
-        if (perguntas[indice].repetida === '') {
+        if (perguntas[indice].repetida === false) {
             // Setando pergunta como repetida
-            perguntas[indice].repetida = 'sim';
+            perguntas[indice].repetida = true;
             return indice
         } else if (tentativas > 15) return indice = 999;
     }
@@ -265,6 +467,8 @@ function geraPergunta() {
     }
 
     enunciado.innerText = perguntas[indice].pergunta;
+    resultado.innerText = `Que pena!! A resposta correta era: ${perguntas[indice].opcoes[perguntas[indice].resposta]}`;
+
     for (let i = 0; i < alternativas.length; i++) {
         alternativas[i].innerHTML = perguntas[indice].opcoes[i];
     }
@@ -320,15 +524,15 @@ async function verifica() {
     if (opcao === respostaAtual) {
         resultado.classList.remove('inativo');
         resultado.innerText = 'Parabéns! Você acertou!';
-        await new Promise(r => setTimeout(r, 2000));
+        await new Promise(r => setTimeout(r, 3000));
         numeroPergunta += 1;
         resultado.classList.add('ativo');
-        if (numeroPergunta > 6) finalizar();
+        if (numeroPergunta > perguntas.length) finalizar();
         geraPergunta();
 
     } else if (opcao !== respostaAtual && alternativaSelecionada !== "") {
         resultado.classList.remove('inativo');
-        resultado.innerText = 'Que pena! Você errou!';
+        await new Promise(r => setTimeout(r, 3000));
         resultado.classList.add('ativo');
         finalizar();
 
@@ -354,9 +558,9 @@ function finalizar() {
     telaQuiz.classList.remove('ativo');
     telaQuiz.classList.add('inativo');
     telaFinal.classList.add('ativo');
-    resultadoPerguntas.innerText = `Você conseguiu responder ${numeroPergunta-1} de 6 perguntas e faturou...`;
+    resultadoPerguntas.innerText = `Você conseguiu responder ${numeroPergunta-1} de ${informacoes.length} perguntas e faturou...`;
     if (numeroPergunta-1 === 0) resultadoPremio.innerText = `R$0,00 reais! Que pena :(`;
-    else if (numeroPergunta >= 6) resultadoPremio.innerText = `R$1.000.000,00 de reais! Parabéns!`;
+    else if (numeroPergunta >= informacoes.length) resultadoPremio.innerText = `R$1.000.000,00 de reais! Parabéns!`;
     resultadoPremio.innerText = `${informacoes[numeroPergunta-1].premioDerrota} reais!`;
 }
 
@@ -369,6 +573,4 @@ function limpar() {
     resultado.classList.remove('ativo');
     resultado.classList.add('inativo');
 }
-
-
 iniciar();
